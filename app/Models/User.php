f055301562cc709db2de48d47186d5793e7d9331
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2024-03-14 16:09:57 +0300

    WebAuthn FingerPrint Authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laragear\WebAuthn\Contracts\WebAuthnAuthenticatable;
+use Laragear\WebAuthn\WebAuthnAuthentication;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    use WebAuthnAuthentication;
 
     /**
      * The attributes that are mass assignable.
